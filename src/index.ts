b1373089a7a94dcecf18b6e0da13271518c17aeb
abulyousuf <abulfmyousuf@gmail.com>
2022-09-23 20:56:03 +0100

    Check the basic data model features

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+
+let todos: TodoItem[] = [
+  new TodoItem(1, "Buy Milk"),
+  new TodoItem(2, "Make Tea"),
+  new TodoItem(3, "Collect Parcel"),
+  new TodoItem(4, "Call Joe", true),
+];
+
+let collection: TodoCollection = new TodoCollection("Abul", todos);
+
 console.clear();
-console.log("Abul's Todo List");
+console.log(`${collection.userName}'s Todo List`);
+
+let newId: number = collection.addTodo("Go for walk");
+let todoItem: TodoItem = collection.getTodoById(newId);
+
+todoItem.printDetails();
